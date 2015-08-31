3963f2a867934393640c4c9ad2c65cc501721df4
kandros <kandros5591@gmail.com>
2015-09-01 00:14:29 +0200

    initial commit

---
+++ weatherNode.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var luogo = process.argv[2];
+var url = "http://api.openweathermap.org/data/2.5/weather?q="+ luogo +",it&units=metric";
+
+http.get(url, function(res) {
+  body = "";
+  res.setEncoding('utf8');
+  res.on("data", function(chunk) {
+    body += chunk;
+  });
+  res.on("end", function(){
+    var jsonresult = JSON.parse(body);
+    console.log("Temperatura attuale a" + luogo + " : " + jsonresult.main.temp + "°C");
+  });
+});
+
